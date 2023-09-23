18231e099342c522f10e71d4683790bf2aa054ae
SilasSenai <145858740+SilasSenai@users.noreply.github.com>
2023-09-23 17:19:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# dinamica / interpretada / 00P / alto nível
+# conversões = boas práticas
+# nomear de forma semânticas = dar o nome daquilo que o objeto faz
+# utilizar snake case
+# inglês = o código precisa ser universal
+# crie um sistema que mostre um texto na tela
+
+
+# numero = 10
+# numero2 = 2
+# numero3 = numero + numero2
+# print(numero3)
+
+
+# nome = input("digite o seu nome\n")
+# print("Olá,tudo bem", nome)
+
+
+# print("Calculadora de soma")
+# n1 = int(input("digite um número\n"))
+# n2 = int(input("Digite o número 2\n"))
+# n3 = n1 + n2
+# print("O resultado da soma é", n3)
+
+nome = "Silas"
+print(nome)
+print("\n")
+print("Soma dos números")
+n1 = int(input("digite o primeiro número\n"))
+n2 = int(input("Digite o segundo número\n"))
+n3 = n1 + n2
+print("O resultado da soma é", n3)
+print("\n")
+cumprimento = input("Qual o seu nome\n")
+print("Boa tarde", cumprimento,",",  "Tudo bem\n")
+print("\n")
+print("Multiplicação")
+n1 = int(input("digite o primeiro número\n"))
+n2 = int(input("Digite o segundo número 2\n"))
+n4 = n1 * n2
+print("O resultado da multiplicação é", n4)
